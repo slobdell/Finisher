1a9f0dd7c5dd6c1c3d5c95e0cabdfbeab665ba30
Scott Lobdell <slobdell@uber.com>
2015-09-25 16:22:24 -0700

    update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='finisher',
-    version="0.1.0",
+    version="0.1.1",
     url='https://github.com/slobdell/Finisher',
     author="Scott Lobdell",
     author_email="scott.lobdell@gmail.com",
